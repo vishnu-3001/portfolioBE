9484811c65d0bd1a524d6a9ab81f6cea861600c0
vishnu sai kotha <43777886+vishnu-3001@users.noreply.github.com>
2025-06-17 17:32:29 -0400

    Add files via upload

---
+++ portfolio-backend/src/test/java/com/vishnu/portfolio_backend/PortfolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishnu.portfolio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
